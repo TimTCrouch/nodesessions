8ad41f84905026d0d885c9de6dbc864a03f8e262
Tim Crouch <dacypher@gmail.com>
2018-07-11 21:55:58 -0500

    Fixed up FS7

--- session1/fs7.js
+++ session1/fs7.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
+//using another built-in node package
 const utility = require('util');
 
+//convert the error-first callback function to use promises
 const readFilePromise = utility.promisify(fs.readFile);
 
+//wrap in an async function to use async/await
 async function readFile() {
     try {
         let data = await readFilePromise('users/cool_people/coolFolks.txt');
         console.log(data.toString());
     } catch (ex) {
-
+        console.log(`An error occurred while attempting to open file: ${ex}`);
     }
 }
 
